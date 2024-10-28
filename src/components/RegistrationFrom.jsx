9003b2a7f667c4379f918edb0ed03e19690fd680
Shivankit Agarwal <shivankit1210@gmail.com>
2024-10-29 00:49:24 +0530

    added hamburger feature

--- src/components/RegistrationFrom.jsx
+++ src/components/RegistrationFrom.jsx
@@ -49,7 +49,7 @@ const RegistrationFrom = () => {
       
       <div className='bg-slate-200 h-[90vh] w-screen   flex justify-center items-center flex-col'>
       <p className='text-slate-500 px-5 py-2 text-sm md:text-sm  text-center'>I built this mini registration form to get hands-on experience with handling user input and validating forms in React. I wanted to learn how to manage form state effectively, handle real-time feedback like password matching, and control form submission with user-friendly error messages.</p>
-        <form onSubmit={handleSubmit} className='w-[55vw] h-[60vh] bg-white  text-black md:h-[62vh] md:w-[25vw] rounded-md flex flex-col  px-5 p-5  '>
+        <form onSubmit={handleSubmit} className='w-[70vw] h-[55vh] bg-white  text-black md:h-[62vh] md:w-[25vw] rounded-md flex flex-col  px-5 p-5  '>
             <label className='font-semibold'  htmlFor="email">Email</label> 
             <input className='border-b-1 border-l-0 border-r-1 border-t-0 border-slate-600 border text-slate-600 font-light  outline-none rounded-md  px-1 py-1 ' type="email" name='email' value={data.email} onChange={HandleChange} /> <br />
             
