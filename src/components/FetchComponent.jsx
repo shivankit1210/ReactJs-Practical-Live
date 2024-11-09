9991bb83ba00091f59cd4b559d9fd451539314fa
Shivankit Agarwal <shivankit1210@gmail.com>
2024-11-09 17:46:34 +0530

    changes

--- src/components/FetchComponent.jsx
+++ src/components/FetchComponent.jsx
@@ -9,7 +9,7 @@ const FetchComponent = () => {
 
         const fetchUser = async () => {
             try {
-              const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+              const response = await axios.get("https://fakestoreapi.com/products");
               console.log(response.data)
               setData(response.data);
              
@@ -19,23 +19,35 @@ const FetchComponent = () => {
             }
           };
 
+          
+          // https://jsonplaceholder.typicode.com/users
+
 
 
 
  
 
   return (
-    <div className='flex flex-col  place-content-center items-center bg-gray-900  w-[100%] h-[90vh] text-red-600 overflow-hidden p-7 text-lg font-bold fetch-comp'>
+    <div className='flex flex-col  place-content-center items-center bg-gray-900  w-[100%] text-red-600   text-lg font-bold fetch-comp'>
       <p className='text-slate-300 px-5 py-2 text-sm md:text-sm  text-center font-light'>
       I implemented data fetching with Axios in React to learn how to work with APIs and handle asynchronous data. This project helped me practice using Axios to send requests, process responses, and manage loading states within the UI. By working through this, I learned how to structure API calls, handle errors gracefully, and integrate dynamic data into a React component. 
       </p>
       <button className='bg-red-500 w-48 rounded-md text-white font-semibold'  onClick={fetchUser}>Click to fetch data</button>
+      <div className="flex flex-wrap gap-5  place-content-center items-center mt-20 ">
       {
       data.map((e)=>
-        (<li className="text-white font-light p-2 flex flex-col "  key={e.id}>Email of {e.name} from API is "{e.email}" </li>
-        )
+      (
+        <div className="w-96 h-56 bg-white shadow-sm  shadow-white flex flex-col flex-w p-1 rounded-lg hover:shadow-lg">
+          <span className="text-md text-center text-black">{e.title}</span>
+          <span className="text-sm text-gray-500 font-light">{e.description}</span>
+          
+        </div>
+      )
       )
       }
+
+      </div>
+     
       
     </div>
   );
